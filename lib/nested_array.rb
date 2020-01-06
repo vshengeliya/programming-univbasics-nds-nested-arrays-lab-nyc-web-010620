1a98f15d95208a2f7c3abc407cc84c7349940310
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-06 02:15:31 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,12 +31,12 @@ def array_literal_matrix=
   # alphabetically by the first character.
 end
 
-# def matrix_lookup(matrix, row, column) =
-#   ["Don Gately","Joelle van Dyne","Pat M.","Kate Gompert","Bruc Green"],
-#   [ "Hal Incandenza","Lyle","Gerhard Schtitt","Mario Incandenza","Michael Pemulis" ]
-#   matrix_lookup[1][1]
-#   # Return the matrix's content at row and column
-# end
+def matrix_lookup(matrix, row, column) =
+  ["Don Gately","Joelle van Dyne","Pat M.","Kate Gompert","Bruc Green"],
+  [ "Hal Incandenza","Lyle","Gerhard Schtitt","Mario Incandenza","Michael Pemulis" ]
+  matrix_lookup[1][1]
+  # Return the matrix's content at row and column
+end
 
 # def matrix_update(matrix, row, column, new_value) =
 #   ["Don Gately","Joelle van Dyne","Pat M.","Kate Gompert","Bruc Green"],

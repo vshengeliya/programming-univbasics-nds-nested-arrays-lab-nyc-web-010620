2cc31c55f356dbc1c3cb9fb34955bace33df75cf
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-06 02:13:44 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -19,37 +19,37 @@ ENFIELD_TENNIS_ACADEMY = [
 def assembled_matrix
   
   assembled_matrix = 
-  ["Don Gately","Joelle van Dyne","Pat M.","Kate Gompert","Bruc Green"],
-  [ "Hal Incandenza","Lyle","Gerhard Schtitt","Mario Incandenza","Michael Pemulis"]
+  [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
+
   # Build an array that contains (or, "nests") the residents of The Ennet House
   # and the Enfield Tennis Academy as provided by the constants
 end
 
-def array_literal_matrix= 
-  ["Don Gately","Joelle van Dyne","Pat M.","Kate Gompert","Bruc Green"].sort,
-  [ "Hal Incandenza","Lyle","Gerhard Schtitt","Mario Incandenza","Michael Pemulis"].sort
+# def array_literal_matrix= 
+#   ["Don Gately","Joelle van Dyne","Pat M.","Kate Gompert","Bruc Green"].sort,
+#   [ "Hal Incandenza","Lyle","Gerhard Schtitt","Mario Incandenza","Michael Pemulis"].sort
   
-  #or array_literal_matrix.sort
+#   #or array_literal_matrix.sort
   
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
-  # alphabetically by the first character.
-end
+#   # Using Array literal syntax only, build a nested array that uses the data in
+#   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
+#   # alphabetically by the first character.
+# end
 
-def matrix_lookup(matrix, row, column) =
-  ["Don Gately","Joelle van Dyne","Pat M.","Kate Gompert","Bruc Green"],
-  [ "Hal Incandenza","Lyle","Gerhard Schtitt","Mario Incandenza","Michael Pemulis" ]
-  matrix_lookup[1][1]
-  # Return the matrix's content at row and column
-end
+# def matrix_lookup(matrix, row, column) =
+#   ["Don Gately","Joelle van Dyne","Pat M.","Kate Gompert","Bruc Green"],
+#   [ "Hal Incandenza","Lyle","Gerhard Schtitt","Mario Incandenza","Michael Pemulis" ]
+#   matrix_lookup[1][1]
+#   # Return the matrix's content at row and column
+# end
 
-def matrix_update(matrix, row, column, new_value) =
-  ["Don Gately","Joelle van Dyne","Pat M.","Kate Gompert","Bruc Green"],
-  [ "Hal Incandenza","Lyle","Gerhard Schtitt","Mario Incandenza","Michael Pemulis" ]
+# def matrix_update(matrix, row, column, new_value) =
+#   ["Don Gately","Joelle van Dyne","Pat M.","Kate Gompert","Bruc Green"],
+#   [ "Hal Incandenza","Lyle","Gerhard Schtitt","Mario Incandenza","Michael Pemulis" ]
   
-  matrix_update[0][2]= "Tiny Ewell"
-  matrix_update
+#   matrix_update[0][2]= "Tiny Ewell"
+#   matrix_update
   
-  # Update the matrix location at row and column to have the value of new_value
-  # Return the updated matrix
-end
+#   # Update the matrix location at row and column to have the value of new_value
+#   # Return the updated matrix
+# end

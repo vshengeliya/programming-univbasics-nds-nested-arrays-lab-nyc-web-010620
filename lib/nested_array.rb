730a1d3fbd1197e239eaf7edb383a8b0151d189e
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-06 02:17:22 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,39 +17,33 @@ ENFIELD_TENNIS_ACADEMY = [
 ]
 
 def assembled_matrix
-  
-  assembled_matrix = 
-  ["Don Gately","Joelle van Dyne","Pat M.","Kate Gompert","Bruc Green"],
-  [ "Hal Incandenza","Lyle","Gerhard Schtitt","Mario Incandenza","Michael Pemulis"]
+  [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
+
   # Build an array that contains (or, "nests") the residents of The Ennet House
   # and the Enfield Tennis Academy as provided by the constants
 end
 
-def array_literal_matrix= 
-  ["Don Gately","Joelle van Dyne","Pat M.","Kate Gompert","Bruc Green"].sort,
-  [ "Hal Incandenza","Lyle","Gerhard Schtitt","Mario Incandenza","Michael Pemulis"].sort
-  
-  #or array_literal_matrix.sort
+def array_literal_matrix
+  [ENNET_HOUSE.sort, ENFIELD_TENNIS_ACADEMY.sort]
   
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
 end
 
-def matrix_lookup(matrix, row, column) =
-  ["Don Gately","Joelle van Dyne","Pat M.","Kate Gompert","Bruc Green"],
-  [ "Hal Incandenza","Lyle","Gerhard Schtitt","Mario Incandenza","Michael Pemulis" ]
-  matrix_lookup[1][1]
+def matrix_lookup(matrix, row, column)
   # Return the matrix's content at row and column
+  matrix[row][column]
 end
 
-def matrix_update(matrix, row, column, new_value) =
-  ["Don Gately","Joelle van Dyne","Pat M.","Kate Gompert","Bruc Green"],
-  [ "Hal Incandenza","Lyle","Gerhard Schtitt","Mario Incandenza","Michael Pemulis" ]
-  
-  matrix_update[0][2]= "Tiny Ewell"
-  matrix_update
-  
+def matrix_update(matrix, row, column, new_value)
+  # Return the matrix's content at row and column
+  matrix[row][column]
+end
+
+def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
+  matrix[row][column] = new_value
+  matrix
 end
